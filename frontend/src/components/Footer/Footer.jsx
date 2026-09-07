efca943d9fe6d995400831085f9df107c1632ea1
chandrikadurga <chandrikadurga6@gmail.com>
2026-09-07 16:59:38 +0530

    Update footer logo with circular brand seal emblem

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import logoImg from '../../assets/logo.jpeg';
+import logoImg from '../../assets/footer-logo.png';
 import './Footer.css';
 
 export default function FooterGlow({
